da0d458f097e495f4c17c96ebcbb82cd7ec77d2e
BaymaxGithub <fanqy@inhand.com.cn>
2017-12-09 22:33:36 +0800

    新增user

---
+++ common/src/main/java/com/classroom/zhu/common/model/User.java
@@ -0,0 +1,20 @@
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+public class User {
+    @Id
+    private ObjectId id;   //这个就是uid
+    private ObjectId oid;
+
+    private String email;
+    private String name;
+    private String phone;
+    private Date createTime;
+    private Date updateTime;
+
+    private Date repeat;  //有人重复注册时更新
+}
---
+++ common/src/main/java/com/classroom/zhu/common/model/UserDb.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+//用来在ABCDE_db数据库中保存所有用户的信息
+@Data
+public class UserDb {
+    private String username;
+    private ObjectId oid;
+    private ObjectId uid;
+    private String dbName;
+    private String type; // phone or email
+}
